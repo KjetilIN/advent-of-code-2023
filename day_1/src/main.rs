c69a53cd28f6af15678e84f078c401fd084c32f7
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-12-01 17:17:46 +0100

    add: hashmap with values for part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,7 +1,4 @@
-use std::{fs::File, path::Path, io::{BufReader, Read}};
-
-
-
+use std::{fs::File, path::Path, io::{BufReader, Read}, collections::HashMap};
 
 /**
  * Calculate the number for a given string with random chars
@@ -31,10 +28,54 @@ fn number_from_string(input: &str) -> u32{
     first_digit*10 + last_digit
 }
 
+fn number_from_string_with_number_strings(input: &str) ->u32{
+    // Save the result in a variable 
+    let mut first_digit:u32 = 0;
+    let mut index_first: usize = 0;
+    let mut index_last: usize = 0; 
+    let mut last_digit:u32 = 0;
+
+    // Iterate over the chars 
+    for (index,ch) in input.chars().enumerate(){
+        // Check if the char is numeric
+        if ch.is_numeric(){
+            // Set the first digit if it has not been set
+            if first_digit == 0{
+                first_digit = ch.to_digit(10).unwrap();
+                index_first = index;
+            }
+            // Reset the last number 
+            last_digit = ch.to_digit(10).unwrap();
+            index_last = index;
+        }
+    }
+
+    // Check for any numbers 
+
+   
+
+    // Return the result as a two digit number
+    first_digit*10 + last_digit
+
+    
+}
+
 
 fn main() -> std::io::Result<()>{
     // Print context line
     println!("--- Day 1 ---");
+
+    // Create hashmap for part 2
+    let mut value_map = HashMap::new();
+    value_map.insert("one", 1);
+    value_map.insert("two", 2);
+    value_map.insert("three", 3);
+    value_map.insert("four", 4);
+    value_map.insert("five", 5);
+    value_map.insert("six", 6);
+    value_map.insert("seven", 7);
+    value_map.insert("eight", 8);
+    value_map.insert("nine", 9);
 
     // We store the total sum of the file to a variable 
     let mut total_sum_from_file: u32 = 0;
@@ -60,8 +101,8 @@ fn main() -> std::io::Result<()>{
 
     }
 
-    // Print the result
-    println!("Result: {}", total_sum_from_file);
+    // Print the result for part 1 
+    println!("PART 1: {}", total_sum_from_file);
     
     // Return OK
     Ok(())
