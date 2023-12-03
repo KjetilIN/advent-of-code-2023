09ed7b33a15b19e75d64c1ff38a245a2595f9290
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-12-03 18:29:19 +0100

    add: part 2 solution

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,8 +1,6 @@
 use std::{path::Path, fs::File, io::{self, BufReader, Read}, process::exit};
 
-
-
-// Constant that describe how many of each there is a max number of
+// Constant that describe how many of each there is a max number of each color 
 const MAX_POSSIBLE_REDS: u32 = 12;
 const MAX_POSSIBLE_GREENS: u32 = 13;
 const MAX_POSSIBLE_BLUES: u32 = 14;
@@ -52,8 +50,6 @@ fn get_game_id(vector: &Vec<&str>, index: usize) -> u32{
 
     }
 }
-
-
 
 
 /**
@@ -144,6 +140,71 @@ pub fn check_game_possible(input: &str) -> u32{
 }
 
 
+/**
+ * Function that find the least amount of cubes needed to show for each color. 
+ * - Finds the least amount of each color 
+ * - returns each minimum of colors cubed
+ * - will return 0 if a cube of the three colors was not shown in a game
+ */
+pub fn minimum_cubes_powered(input: &str) -> u32{
+    
+    // Set the number of cubes to be a really high number
+    let mut green: u32 = 0;
+    let mut red: u32 = 0;
+    let mut blue: u32 = 0;
+
+    // We format the game by 
+    // - removing the commas 
+    // - make the semicolons be on their own with spaces
+    let formatted_game = input.trim()
+                                 .replace(",", "")
+                                 .replace(";", " ;");
+
+    // Turn the line into a vector 
+    let word_list: Vec<_> = formatted_game.split(" ").collect();
+
+    for (index, item) in word_list.iter().enumerate(){
+        match item{
+            &"green" => {
+                // Get the amount for the color
+                let amount: u32 =  get_amount(&word_list, index);
+
+                // If the amount is more
+                if amount > green{
+                    green = amount;
+                }
+
+            },
+            &"blue" => {
+                // Get the amount of the color blue
+                let amount: u32 =  get_amount(&word_list, index);
+
+                // If the amount is more
+                if amount > blue{
+                    blue = amount;
+                }
+
+            },
+            &"red" => {
+                // Get the amount of reds
+                let amount: u32 =  get_amount(&word_list, index);
+
+                // If the amount is more
+                if amount > red{
+                    red = amount;
+                }
+
+            }
+            _ => {}
+        }
+    }
+
+
+    // Return the power of each 
+    red*green*blue
+}
+
+
 fn main() -> io::Result<()>{
     println!("--- Day 2 ---");
 
@@ -163,12 +224,20 @@ fn main() -> io::Result<()>{
     // Count that is the sum of games that was possible
     let mut game_ids_possible_sum: u32 = 0;
 
+    // Sum of powers for part 2
+    let mut minimal_cubes_power_sum: u32 = 0;
+
     // For each line
     for line in content.lines(){
         game_ids_possible_sum += check_game_possible(line);
+        minimal_cubes_power_sum += minimum_cubes_powered(line);
     }
 
+    // Print part 1 solution
     println!("Sum of IDs: {game_ids_possible_sum}");
+
+    // Print part 2 solution 
+    println!("Sum of minimal amount of cubes per game powered: {minimal_cubes_power_sum}");
 
 
     Ok(())
