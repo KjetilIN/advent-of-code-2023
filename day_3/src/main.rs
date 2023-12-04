8ce28156a3746450ae57fb715881b12c8b74d88d
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-12-04 20:46:38 +0100

    wip: engine schema map methods

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,13 +1,16 @@
 use std::{fs::File, io::BufReader};
 use std::io::Read;
 
-struct Map<'a>{
+/**
+ * The struct that represent an engine schema 
+ */
+struct EngineSchema<'a>{
     rows: &'a Vec<String>,
     col_length: u32,
     row_length: u32
 }
 
-impl<'a> Map<'a>{
+impl<'a> EngineSchema<'a>{
 
     fn new(rows: &'a Vec<String>) -> Self{
         let row_length = rows.len() as u32;
@@ -15,9 +18,41 @@ impl<'a> Map<'a>{
 
         Self { rows, col_length, row_length }
     }
+    /**
+     * Method that gets the total part sum for all of the parts in the constructed map
+     * Returns the result of all the parts 
+     */
+    fn get_total_part_sum(&self) -> u32{
+        let mut result: u32 = 0;
+
+        for (row_index, row) in self.rows.iter().enumerate(){
+            for (char_index, ch) in row.chars().enumerate(){
+                // If it is a special char, we have found a special char
+                if !ch.is_alphanumeric() || ch != '.'{
+                    result += get_part_sum(row_index, char_index);
+                }
+            }
+        }
+
+        result
+    }
+
+
     
 }
 
+/**
+ * Function that takes finds the sum part for a single part
+ * Takes the row index and char index of the given part 
+ * Returns the sum or 0 if no parts for that sum is available 
+ */
+pub fn get_part_sum(row_index:usize, char_index:usize) -> u32{
+    let mut sum = 0;
+
+
+
+    sum
+}
 
 
 fn main() -> std::io::Result<()> {
@@ -32,6 +67,16 @@ fn main() -> std::io::Result<()> {
         eprintln!("Error reading the map file : {err}");
         panic!();
     });
+
+    let mut parts_vector = Vec::<String>::new();
+    for line in content.lines(){
+        parts_vector.push(line.to_string().clone());
+    }
+
+    let parts_map = EngineSchema::new(&parts_vector);
+   
+
+    println!("Parts Map size: {}x{}", parts_map.col_length, parts_map.row_length);
 
 
     Ok(())
