a1b039fcab74bf9e397b58104b40f62f63e83219
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-12-05 13:34:23 +0100

    add: is_part_number and get_part_sum WIP

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,9 +1,10 @@
-use std::result;
 use std::{fs::File, io::BufReader};
 use std::io::Read;
 
 /**
- * The struct that represent an engine schema 
+    The struct that represent an engine schema 
+    - Has the vectors of all the rows
+    - Has the length of columns and rows
  */
 struct EngineSchema<'a>{
     rows: &'a Vec<String>,
@@ -14,59 +15,136 @@ struct EngineSchema<'a>{
 impl<'a> EngineSchema<'a>{  
 
     /**
-     * Constructor function for creating a new row
+        Constructor function for creating a new Engine Schema Struct
+        - Takes a vector of strings
+        - The life time of the struct is the same as the Vector given
     */
 
     fn new(rows: &'a Vec<String>) -> Self{
-        let row_length = rows.len() as u32;
+        let row_length = rows.len().clone() as u32;
         let col_length = rows.get(0).unwrap().len().clone() as u32;
 
         Self { rows, col_length, row_length }
     }
 
+
     /**
-     * FUNCTIONS FOR CHECKING IF THERE ARE ANY NUMBERS 
+     * Method that gets the char from the map vector.
+     * - Gets the indexes 
+     * 
+     * Returns an option with the char or none if illegal indexes where given 
     */
+    fn get_char_from_vector(&self, row_index:usize, char_index:usize) -> Option<char>{
 
-    pub fn is_part_top_left(row_index:usize, char_index:usize) -> bool{
-        todo!();
+        // Validating input or panics 
+        if row_index > self.row_length.try_into().unwrap() {
+            eprintln!("ERROR: illegal row index given: {row_index}");
+            return None;
+        }else if char_index > self.col_length.try_into().unwrap(){
+            eprintln!("ERROR: illegal char index given: {char_index}");
+            return None;
+        }
+
+        // Get row from the option
+        let binding = self.rows.get(row_index);
+        let row = match &binding{
+            Some(row) => row,
+            None => return None,
+        };
+        let row_chars: Vec<_> = row.chars().collect();
+
+        // return the option
+        Some(row_chars[char_index])
+    }
+
+
+    /**
+     * Method that checks if a given index is a part number / aka a digit
+     * - Takes the row index and char index of the char we want to check
+     * - Checks that the given indexes could be an item on the map
+     * - (Uses get_char_from_vector method for validating)
+     * 
+     * Returns true if the item is a number
+     */
+    pub fn is_part_number(&self, row_index:usize, char_index:usize) -> bool{
+        match self.get_char_from_vector(row_index, char_index){
+            Some(item) => item.is_numeric(),
+            None => false
+        }
     }   
-
-    pub fn is_part_top_right(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }   
-
-    pub fn is_part_top(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }   
-
-    pub fn is_part_left(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }   
-
-    pub fn is_part_right(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }   
-
-    pub fn is_part_bottom_left(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }   
-
-    pub fn is_part_bottom_right(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }   
-
-    pub fn is_part_bottom(row_index:usize, char_index:usize) -> bool{
-        todo!();
-    }  
 
     /**
      * Function that takes finds the sum part for a single part
      * Takes the row index and char index of the given part 
      * Returns the sum or 0 if no parts for that sum is available 
     */
-    pub fn get_part_sum(row_index:usize, char_index:usize) -> u32{
+    pub fn get_part_sum(&self, row_index:usize, char_index:usize) -> u32{
         let mut sum = 0;
+
+        // Declare variables for the indexes
+        let top_row_index = row_index - 1;
+        let bottom_row_index = row_index + 1;
+        let left_char_index = char_index - 1;
+        let right_char_index = char_index - 1;
+
+        // Check all sides for the number
+        let number_top = self.is_part_number(top_row_index, char_index);
+        let number_top_left = self.is_part_number(top_row_index, left_char_index);
+        let number_top_right = self.is_part_number(top_row_index, right_char_index);
+        let number_left = self.is_part_number(row_index, left_char_index);
+        let number_right = self.is_part_number(row_index, right_char_index);
+        let number_bottom = self.is_part_number(bottom_row_index, char_index);
+        let number_bottom_right = self.is_part_number(bottom_row_index, right_char_index);
+        let number_bottom_left = self.is_part_number(bottom_row_index, left_char_index);
+
+        
+
+
+        // Check the top
+        if number_top{
+            let top_number = get_number(self.rows.get(top_row_index).unwrap(), char_index);
+            sum += top_number;
+
+        }else{
+            // There is no number on the top, 
+            // and if there are numbers to the left and right, we know that they are not the same numbers 
+            // There is a space between the numbers
+            if number_top_left {
+                sum += get_number(self.rows.get(top_row_index).unwrap(), left_char_index);
+            }
+
+            if number_top_right {
+                sum += get_number(self.rows.get(top_row_index).unwrap(), right_char_index);
+            }
+        }
+
+        // Check both sides 
+        if number_left{
+            sum += get_number(self.rows.get(row_index).unwrap(), left_char_index);
+        }
+
+        if number_right{
+            sum += get_number(self.rows.get(row_index).unwrap(), right_char_index);
+        }
+
+
+        // Check the top
+        if number_bottom{
+            let bottom_number = get_number(self.rows.get(bottom_row_index).unwrap(), char_index);
+            sum += bottom_number;
+
+        }else{
+            // There is no number on the top, 
+            // and if there are numbers to the left and right, we know that they are not the same numbers 
+            // There is a space between the numbers
+            if number_bottom_left {
+                sum += get_number(self.rows.get(bottom_row_index).unwrap(), left_char_index);
+            }
+
+            if number_bottom_right {
+                sum += get_number(self.rows.get(bottom_row_index).unwrap(), right_char_index);
+            }
+        }
 
 
 
@@ -85,7 +163,7 @@ impl<'a> EngineSchema<'a>{
             for (char_index, ch) in row.chars().enumerate(){
                 // If it is a special char, we have found a special char
                 if !ch.is_alphanumeric() || ch != '.'{
-                    result += Self::get_part_sum(row_index, char_index);
+                    result += Self::get_part_sum(self, row_index, char_index);
                 }
             }
         }
@@ -108,11 +186,9 @@ impl<'a> EngineSchema<'a>{
  * Returns the number parsed
  * 
  */
-pub fn get_number(number_string: &String, number_index:usize) -> u32{
-    // Create a new string
-
-    let char_vector: Vec<_> = number_string.chars().collect();
-    
+pub fn get_number(row: &String, number_index:usize) -> u32{
+    // Create a new vector of chars from the string row
+    let char_vector: Vec<_> = row.chars().collect();
 
     // Check if the start index is numeric, else print the error and return 0 
     if !char_vector[number_index].is_numeric(){
@@ -122,7 +198,6 @@ pub fn get_number(number_string: &String, number_index:usize) -> u32{
     
     // Save the central char as a variable 
     let central_char = char_vector[number_index];
-    println!("Central char: {}", central_char);
 
     //We add each numeric char that is to the left
     let mut right_index = number_index + 1;
@@ -133,8 +208,6 @@ pub fn get_number(number_string: &String, number_index:usize) -> u32{
         right_index += 1;
     };
 
-    println!("RIGHT: '{right_number}'");
-
     // Then we get all the chars from the front 
     let mut left_index = number_index - 1;
     let mut left_number = String::new();
@@ -143,8 +216,6 @@ pub fn get_number(number_string: &String, number_index:usize) -> u32{
         left_number.push(char_vector[left_index]);
         left_index-=1;
     };
-
-    println!("LEFT: '{left_number}'");
 
 
     // Format the result as a string 
@@ -155,8 +226,6 @@ pub fn get_number(number_string: &String, number_index:usize) -> u32{
     } else {
         format!("{}{}{}", left_number.chars().rev().collect::<String>(),central_char,right_number)
     };
-
-    println!("RESULT STING: {result_string}");
 
     match (result_string).parse(){
         Ok(number) => number,
