aebc0f5ff23d17c9c7dc973ec040bf90de8a5de2
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-12-17 18:46:05 +0100

    del: old implementation of Rc<RefCell<Node>>

--- day_8/src/tree_methods.rs
+++
@@ -1,7 +0,0 @@
-use std::{collections::HashMap, rc::Rc, cell::RefCell, borrow::BorrowMut};
-
-use crate::node::{Node, NodeMethods, NodeReference};
-
-pub fn build_tree_from_map(root: &mut Rc<RefCell<Node>>, nodes: &mut HashMap<String, (String, String)>) {
-    todo!()
-}
